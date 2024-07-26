469828243c5221123cf75bdc851db018d14cecd7
Andzuo <alucasm02@gmail.com>
2024-07-26 19:38:48 -0300

    chore: define color variables

---
+++ src/themes.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+   palette: {
+      primary: {
+         main: "#FFB800",
+      },
+      secondary: {
+         main: "#475467",
+      },
+      whatsappButton: {
+         main: "#25D366",
+      },
+   },
+});
