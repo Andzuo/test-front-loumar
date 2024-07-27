3bbd859e8e2c7291439fa26d1f4403dbacaee829
Andzuo <alucasm02@gmail.com>
2024-07-27 14:03:21 -0300

    Refact: Refatorando HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { Box } from "@mui/material";
 import Banner from "../assets/Banner.png";
 
 const HeroSection = () => {
    return (
-      <div>
-         <img src={Banner} alt="banner" />
-      </div>
+      <Box
+         sx={{
+            width: "100%",
+            justifyContent: "center",
+            overflow: "hidden",
+         }}
+      >
+         <Box
+            component="img"
+            src={Banner}
+            alt="banner"
+            sx={{
+               width: "100%",
+            }}
+         />
+      </Box>
    );
 };
 
