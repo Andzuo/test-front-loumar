724a5259b2eb227dc61a6764e451d76f4ee004cb
Andzuo <alucasm02@gmail.com>
2024-07-26 21:38:27 -0300

    feat: add FirstSection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
+import FirstSection from "./components/FirstSection";
 
 const App = () => {
    return (
       <div>
          <NavBar />
          <HeroSection />
+         <FirstSection />
       </div>
    );
 };
