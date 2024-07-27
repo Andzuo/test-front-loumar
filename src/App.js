1f6bd7f871f441e71124a0360fe7aaeda07d023e
Andzuo <alucasm02@gmail.com>
2024-07-26 21:12:56 -0300

    Feat: add heroSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
 
 const App = () => {
    return (
       <div>
          <NavBar />
+         <HeroSection />
       </div>
    );
 };
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../assets/Banner.png";
+
+const HeroSection = () => {
+   return (
+      <div style={{ margin: 0 }}>
+         <img src={Banner} alt="banner" />
+      </div>
+   );
+};
+
+export default HeroSection;
