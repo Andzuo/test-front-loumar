d15824fa5cc35b7be0bf43be69e1bba1c6330e8c
Andzuo <alucasm02@gmail.com>
2024-07-27 09:19:12 -0300

    Feat: ActionSection complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
 import FirstSection from "./components/FirstSection";
 import FaqSection from "./components/FaqSection";
+import ActionPage from "./components/ActionSection";
 
 const App = () => {
    return (
@@ -11,6 +12,7 @@ const App = () => {
          <HeroSection />
          <FirstSection />
          <FaqSection />
+         <ActionPage />
       </div>
    );
 };
--- src/components/ActionSection.jsx
+++ src/components/ActionSection.jsx
@@ -1,10 +1,59 @@
+import styled from "@emotion/styled";
+import { Box, Button, Typography } from "@mui/material";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import React from "react";
+
+const SectionContainer = styled(Box)(({ theme }) => ({
+   display: "flex",
+   flexDirection: "row",
+   alignItems: "center",
+   justifyContent: "center",
+   textAlign: "center",
+   padding: "87px 352px 72px 352px",
+   width: "100%",
+   margin: "0 auto",
+   [theme.breakpoints.down("xl")]: {
+      flexDirection: "column",
+      padding: "16px",
+   },
+}));
+
+const BoxContent = styled(Box)(({ theme }) => ({
+   display: "flex",
+   width: "100%",
+   padding: "48px 32px",
+   flexDirection: "column",
+   alignItems: "center",
+   justifyContent: "center",
+   border: "1px solid #E0E0E0",
+   borderRadius: "16px",
+}));
 
 const ActionPage = () => {
    return (
-      <div>
-         <h1>Action Page</h1>
-      </div>
+      <SectionContainer>
+         <BoxContent>
+            <Typography variant="h6" gutterBottom>
+               Ainda tem dúvidas sobre Pizza?
+            </Typography>
+            <Typography
+               sx={{ margin: "16px 0" }}
+               variant="body2"
+               color="textSecondary"
+            >
+               Entre em contato direto com nossos atendentes.
+            </Typography>
+
+            <Button
+               startIcon={<WhatsAppIcon />}
+               variant="contained"
+               color="whatsappButton1"
+               sx={{ color: "#fff", marginY: "30px" }}
+            >
+               Entrar em contato
+            </Button>
+         </BoxContent>
+      </SectionContainer>
    );
 };
 
