8d7c157bb7f610a93c5a97b317308b76ad580b27
Andzuo <alucasm02@gmail.com>
2024-07-28 18:22:15 -0300

    Refact: refactored logic for section image in the mobile version

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,22 +1,30 @@
 import React from "react";
-import { Paper } from "@mui/material";
+import { useTheme, useMediaQuery } from "@mui/material";
+import styled from "@emotion/styled";
 import Banner from "../assets/Banner.png";
+import BannerMobile from "../assets/BannerMobile.png";
+
+const ImageContainer = styled("div")(({ theme }) => ({
+   display: "flex",
+   width: "100%",
+   height: "26.5625rem",
+   justifyContent: "center",
+   position: "relative",
+   "& img": {
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
+   },
+}));
 
 const HeroSection = () => {
+   const theme = useTheme();
+   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
    return (
-      <Paper
-         sx={{
-            display: "flex",
-            width: "100%",
-            height: "26.5625rem", // 425px convertidos para rem
-            justifyContent: "center",
-            overflow: "hidden",
-            backgroundImage: `url(${Banner})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-         }}
-      ></Paper>
+      <ImageContainer>
+         <img src={isMobile ? BannerMobile : Banner} alt="Banner" />
+      </ImageContainer>
    );
 };
 
